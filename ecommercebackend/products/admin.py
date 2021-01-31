8cb9742cd76912a8af94058dce63953352f39dd6
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-01-31 22:23:46 +0300

    Created admin for product

--- ecommercebackend/products/admin.py
+++ ecommercebackend/products/admin.py
@@ -1,3 +1,25 @@
+"""
+Contains admin pages of models in :module:`products.models`
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, ProductVariant
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`products.Product`
+    """
+
+    search_fields = ("name", "subheading", "description")
+
+
+@admin.register(ProductVariant)
+class ProductVariantAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`products.ProductVariant`
+    """
+
+    search_fields = ("barcode",)
