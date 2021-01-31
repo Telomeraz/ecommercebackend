13dcd342e2de326720b4cc63a2bf0076cc185790
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-01-31 22:48:15 +0300

    Added list_display to admin classes

--- ecommercebackend/products/admin.py
+++ ecommercebackend/products/admin.py
@@ -13,6 +13,16 @@ class ProductAdmin(admin.ModelAdmin):
     Admin page of :model:`products.Product`
     """
 
+    list_display = (
+        "name",
+        "subheading",
+        "description",
+        "currency",
+        "tax_rate",
+        "created_datetime",
+        "is_active",
+        "is_archived",
+    )
     search_fields = ("name", "subheading", "description")
 
 
@@ -22,4 +32,16 @@ class ProductVariantAdmin(admin.ModelAdmin):
     Admin page of :model:`products.ProductVariant`
     """
 
+    list_display = (
+        "product",
+        "barcode",
+        "price",
+        "purchase_price",
+        "list_price",
+        "stock",
+        "created_datetime",
+        "is_primary",
+        "is_active",
+        "is_archived",
+    )
     search_fields = ("barcode",)
