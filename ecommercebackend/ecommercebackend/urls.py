31b611f83157e68f4d586148ceb5d7a497945920
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-02-11 20:09:24 +0300

    Changed api endpoint for product

--- ecommercebackend/ecommercebackend/urls.py
+++ ecommercebackend/ecommercebackend/urls.py
@@ -5,5 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("rest_framework.urls")),
-    path("v1/products/", include("products.api.v1.urls")),
+    path("api/v1/products/", include("products.api.v1.urls")),
 ]
