36c13446ee8b9762989f256e5c4faa02f1e2c749
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-02-14 14:15:23 +0300

    Reverted for fix

--- ecommercebackend/accounts/models.py
+++ ecommercebackend/accounts/models.py
@@ -8,30 +8,16 @@ class Country(models.Model):
     Stores country's infos.
     """
 
-    name = models.CharField(max_length=255, unique=True, verbose_name=_("Country Name"))
+    name = models.CharField(max_length=255, verbose_name=_("Country Name"))
 
-    alpha2_code = models.CharField(max_length=2, unique=True, verbose_name=_("Alpha-2 Code"))
+    alpha2_code = models.CharField(max_length=2, verbose_name=_("Alpha-2 Code"))
 
-    alpha3_code = models.CharField(max_length=3, unique=True, verbose_name=_("Alpha-3 Code"))
+    alpha3_code = models.CharField(max_length=3, verbose_name=_("Alpha-3 Code"))
 
     numeric_code = models.PositiveSmallIntegerField(
         validators=(validators.MaxValueValidator(999),),
-        unique=True,
         verbose_name=_("Numeric Code"),
     )
 
     def __str__(self):
         return self.name
-
-
-class City(models.Model):
-    """
-    Stores city's infos.
-    """
-
-    name = models.CharField(max_length=255, verbose_name=_("City Name"))
-
-    country = models.ForeignKey(Country, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name + ", " + self.country
