c1cbee91eb2ba7739dc4dac8d29840ac2c2af750
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-02-15 21:57:54 +0300

    Added admin pages for Order and OrderLine

--- ecommercebackend/orders/admin.py
+++ ecommercebackend/orders/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, OrderLine
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`orders.Order`
+    """
+
+    list_display = (
+        "id",
+        "customer",
+        "shipping_address",
+        "billing_address",
+        "total",
+        "created_datetime",
+    )
+    search_fields = ("id",)
+
+
+@admin.register(OrderLine)
+class OrderLineAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`orders.OrderLine`
+    """
+
+    list_display = (
+        "id",
+        "order",
+        "product",
+        "package",
+        "status",
+        "currency",
+        "tax_rate",
+        "unit_price",
+        "quantity",
+        "discount_rate",
+        "unit_price_excluding_discount",
+        "unit_price_excluding_tax",
+        "total_price",
+    )
+    search_fields = ("id", "status")
