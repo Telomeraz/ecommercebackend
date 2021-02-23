27dba40439b63b0f9a1144412102cf9e2ac65d7f
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-02-23 22:56:57 +0300

    Created Customer model

--- ecommercebackend/accounts/models.py
+++ ecommercebackend/accounts/models.py
@@ -5,6 +5,8 @@ from django.core import validators
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
+from utils.models import BaseArchive
+
 
 class Country(models.Model):
     """
@@ -48,6 +50,33 @@ class City(models.Model):
 
     def __str__(self):
         return "{}, {}".format(self.name, self.country)
+
+
+class Customer(BaseArchive):
+    """
+    Stores customer infos. This model is used for non-user orders. If an order
+    is third-party software order or manuel order which is created by admin,
+    this model should be used for customer infos of those type of orders.
+    """
+
+    first_name = models.CharField(max_length=255, blank=True, verbose_name=_('First name'))
+
+    last_name = models.CharField(max_length=255, blank=True, verbose_name=_('Last name'))
+
+    email = models.EmailField(_('Email address'), blank=True)
+
+    phone_number = PhoneNumberField(blank=True, verbose_name=_("Contact phone number"))
+
+    class Meta:
+        verbose_name = _("Customer")
+        verbose_name_plural = _("Customers")
+
+    def __str__(self):
+        return self.full_name
+
+    @property
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
 
 
 class Address(models.Model):
