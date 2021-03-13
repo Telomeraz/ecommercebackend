43d63c66f5ae2ef3ed8bf04f6c77a44ec7557b5f
Telomeraz <huseyinemre_armagan@hotmail.com>
2021-03-13 21:56:49 +0300

    Added admin for shipping

--- ecommercebackend/shipping/admin.py
+++ ecommercebackend/shipping/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
 
-# Register your models here.
+from shipping.models import Courier, Package, Shipment
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`shipping.Courier`
+    """
+
+    list_display = ("id", "name")
+    search_fields = ("id", "name")
+
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`shipping.Package`
+    """
+
+    list_display = ("id", "shipment", "created_datetime")
+    search_fields = ("id", "shipment", "created_datetime")
+
+
+@admin.register(Shipment)
+class ShipmentAdmin(admin.ModelAdmin):
+    """
+    Admin page of :model:`shipping.Shipment`
+    """
+
+    list_display = (
+        "id",
+        "courier",
+        "tracking_code",
+        "tracking_url",
+        "fee",
+        "is_buyer_pays",
+    )
+    search_fields = (
+        "id",
+        "courier",
+        "tracking_code",
+        "tracking_url",
+        "fee",
+        "is_buyer_pays",
+    )
